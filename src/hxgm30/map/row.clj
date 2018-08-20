eb3d95a5e118c78ec5f99899323eb6db6d257cfc
Duncan McGreggor <oubiwann@gmail.com>
2018-08-20 00:18:16 -0500

    Use a cheaper operation for first?/last?.

--- src/hxgm30/map/row.clj
+++ src/hxgm30/map/row.clj
@@ -1,6 +1,7 @@
 (ns hxgm30.map.row
   (:require
     [hxgm30.map.bands :as bands]
+    [hxgm30.map.components.config :as config]
     [hxgm30.map.io :as map-io]
     [hxgm30.map.util :as util]
     [taoensso.timbre :as log]))
@@ -11,17 +12,26 @@
 
 (defn create
   [system y]
+  (log/debugf "Creating row data from row index %s ..." y)
   (map->Row {:index y
              :data (->> (for [x (util/xs system)] [x y])
                         (map #(bands/coords->bands system %))
                         (remove #'util/no-band-data?))}))
 
-(defn first?
+(defn first-row-with-data?
   [system row-data]
   (and (util/some-data? (:data row-data))
        (util/no-data? (:data (create system (dec (:index row-data)))))))
 
-(defn last?
+(defn last-row-with-data?
   [system row-data]
   (and (util/some-data? (:data row-data))
        (util/no-data? (:data (create system (inc (:index row-data)))))))
+
+(defn first?
+  [system row]
+  (= (:index row) (config/starting-row system)))
+
+(defn last?
+  [system row]
+  (= (:index row) (config/ending-row system)))
--- test/hxgm30/map/tests/system/row.clj
+++ test/hxgm30/map/tests/system/row.clj
@@ -1,3 +1,23 @@
+(deftest first-row-with-data?
+  (let [r0 (row/create (system) 109)
+        r1 (row/create (system) 110)
+        r2 (row/create (system) 111)
+        r3 (row/create (system) 112)]
+    (is (not (row/first-row-with-data? (system) r0)))
+    (is (not (row/first-row-with-data? (system) r1)))
+    (is (row/first-row-with-data? (system) r2))
+    (is (not (row/first-row-with-data? (system) r3)))))
+
+(deftest last-row-with-data?
+  (let [r4 (row/create (system) 908)
+        r5 (row/create (system) 909)
+        r6 (row/create (system) 910)
+        r7 (row/create (system) 911)]
+    (is (not (row/last-row-with-data? (system) r4)))
+    (is (row/last-row-with-data? (system) r5))
+    (is (not (row/last-row-with-data? (system) r6)))
+    (is (not (row/last-row-with-data? (system) r7)))))
+
 (deftest first-row?
   (let [r0 (row/create (system) 109)
         r1 (row/create (system) 110)
