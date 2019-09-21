d772e3c523f183bd25a5530b6639ca1eb9781d04
Duncan McGreggor <oubiwann@gmail.com>
2019-09-21 18:15:54 -0500

    Added temperature and elevation scale parsing and searching.

--- src/hxgm30/map/scales.clj
+++ src/hxgm30/map/scales.clj
@@ -7,13 +7,17 @@
   (:import
     (java.awt.image BufferedImage)))
 
-(def min-elevation 0)
-(def max-elevation 30000)
-(def min-temperature 213)
-(def max-temperature 333)
+(def min-elevation 0) ; in meters
+(def max-elevation 30000) ; in meters
+(def min-temperature 212) ; in degrees C/K
+(def max-temperature 333) ; in degrees C/K
+
+(def elevation-file "001-mercator-elevation-scale")
+(def temperature-file "001-mercator-temperature-scale")
 
 (defn read-scale
-  ""
+  "Image files are read from the top-down, so high elevations and high numbers
+  will be first."
   [filename]
   (->> filename
        (format map-io/png-format)
@@ -25,24 +29,79 @@
   (let [middle (int (/ (map-io/width im) 2))]
     (map #(map-io/bands im middle %) (range (map-io/height im)))))
 
+(def elevation (read-scale elevation-file))
+(def temperature (read-scale temperature-file))
+
 (defn elevation-colors
   ""
   []
-  (-> "001-mercator-elevation-scale"
-      read-scale
-      scale-colors))
+  (scale-colors elevation))
 
 (defn temperature-colors
   ""
   []
-  (-> "001-mercator-temperature-scale"
-      read-scale
-      scale-colors))
+  (scale-colors temperature))
 
-;; XXX get the number of meters covered in each gradation
+(def meters-per-grade (float (/ (- max-elevation min-elevation)
+                                (dec (map-io/height elevation)))))
 
-;; XXX get the number of degrees Kelvin that are covered in each gradation
+(def degrees-per-grade (float (/ (- max-temperature min-temperature)
+                                 (dec (map-io/height temperature)))))
 
-;; XXX add a function for getting the nearest gradation for a given elevation
+(defn get-ranges
+  ""
+  [start stop step]
+  (map #(vector % (+ % step))
+       (range start stop step)))
 
-;; XXX add a function for getting the nearest gradation for a given temperature
+(defn elevation-ranges
+  ""
+  []
+  (get-ranges min-elevation max-elevation meters-per-grade))
+
+(defn temperature-ranges
+  ""
+  []
+  (get-ranges min-temperature max-temperature degrees-per-grade))
+
+(defn elevation-lookup
+  ""
+  []
+  (->> (elevation-colors)
+       reverse
+       (interleave (elevation-ranges))
+       (partition 2)
+       (map vec)
+       (into {})))
+
+(defn temperature-lookup
+  ""
+  []
+  (->> (temperature-colors)
+       reverse
+       (interleave (temperature-ranges))
+       (partition 2)
+       (map vec)
+       (into {})))
+
+(defn find-range
+  [item collection]
+  (reduce (fn [_ [a b]] (when (<= a item b) (reduced [a b])))
+          []
+          collection))
+
+(defn find-elevation-range
+  [meters]
+  (find-range meters (elevation-ranges)))
+
+(defn find-temperature-range
+  [kelvin]
+  (find-range kelvin (temperature-ranges)))
+
+(defn elevation-color
+  [meters]
+  (get (elevation-lookup) (find-elevation-range meters)))
+
+(defn temperature-color
+  [kelvin]
+  (get (temperature-lookup) (find-temperature-range kelvin)))
