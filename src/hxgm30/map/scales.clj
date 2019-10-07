9db9b1aec9f6a4504141756636bd133976074a0c
Duncan McGreggor <oubiwann@gmail.com>
2019-10-07 00:49:19 -0500

    Re-orged the scales file.

--- src/hxgm30/map/scales.clj
+++ src/hxgm30/map/scales.clj
@@ -9,50 +9,9 @@
   (:import
     (java.awt.image BufferedImage)))
 
-(def elevation-min 0) ; in strides (~2 leagues or ~7 miles)
-(def elevation-max units/highest-mountain) ; in strides (~2 leagues or ~7 miles)
-(def elevation-file "001-mercator-elevation-scale-hex")
-
-(def precipitation-min 0) ; in mils/year (~4500 mm/yr)
-(def precipitation-max 640000) ; in mils/year, ~12,000 mil/week (~16,000 mm/yr)
-(def precipitation-file "001-mercator-precipitation-scale-hex")
-
-(def temperature-min 212) ; in degrees C/K
-(def temperature-max 333) ; in degrees C/K
-(def temperature-file "001-mercator-temperature-scale-hex")
-
-(defn elevation-check
-  [strides]
-  (cond (< strides elevation-min)
-        elevation-min
-
-        (> strides elevation-max)
-        elevation-max
-
-        :else
-        strides))
-
-(defn precipitation-check
-  [milyr]
-  (cond (< milyr precipitation-min)
-        precipitation-min
-
-        (> milyr precipitation-max)
-        precipitation-max
-
-        :else
-        milyr))
-
-(defn temperature-check
-  [kelvin]
-  (cond (< kelvin temperature-min)
-        temperature-min
-
-        (> kelvin temperature-max)
-        temperature-max
-
-        :else
-        kelvin))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   General   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn read-scale-img
   "Image files are read from the top-down, so high elevations and high numbers
@@ -80,51 +39,11 @@
   [lines]
   (map util/hex->color-map lines))
 
-(def elevation (read-scale-txt elevation-file))
-(def temperature (read-scale-txt temperature-file))
-(def precipitation (read-scale-txt precipitation-file))
-
-(def elevation-colors
-  (memoize
-    (fn [] (scale-colors-txt elevation))))
-
-(def temperature-colors
-  (memoize
-    (fn [] (scale-colors-txt temperature))))
-
-(def precipitation-colors
-  (memoize
-    (fn [] (scale-colors-txt precipitation))))
-
-(def strides-per-grade (float (/ (- elevation-max elevation-min)
-                                 (dec (count (elevation-colors))))))
-
-(def degrees-per-grade (float (/ (- temperature-max temperature-min)
-                                 (dec (count (temperature-colors))))))
-
-(def milyr-per-grade (float (/ (- precipitation-max precipitation-min)
-                                  (dec (count (precipitation-colors))))))
-
 (defn get-ranges
   ""
   [start stop step]
   (map #(vector % (+ % step))
        (range start stop step)))
-
-(defn elevation-ranges
-  ""
-  []
-  (get-ranges elevation-min elevation-max strides-per-grade))
-
-(defn temperature-ranges
-  ""
-  []
-  (get-ranges temperature-min temperature-max degrees-per-grade))
-
-(defn precipitation-ranges
-  ""
-  []
-  (get-ranges precipitation-min precipitation-max milyr-per-grade))
 
 (defn- -maybe-reverse
   [opts data]
@@ -141,6 +60,32 @@
          (interleave x-ranges)
          (partition 2))))
 
+(defn find-range
+  [item collection]
+  (reduce (fn [_ [a b]] (when (<= a item b) (reduced [a b])))
+          []
+          collection))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Elevation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def elevation-min 0) ; in strides (~2 leagues or ~7 miles)
+(def elevation-max units/highest-mountain) ; in strides (~2 leagues or ~7 miles)
+(def elevation-file "001-mercator-elevation-scale-hex")
+(def elevation (read-scale-txt elevation-file))
+(def elevation-colors
+  (memoize
+    (fn [] (scale-colors-txt elevation))))
+
+(def strides-per-grade (float (/ (- elevation-max elevation-min)
+                                 (dec (count (elevation-colors))))))
+
+(defn elevation-ranges
+  ""
+  []
+  (get-ranges elevation-min elevation-max strides-per-grade))
+
 (def elevation-lookup
   (memoize
     (fn []
@@ -155,21 +100,77 @@
            (map (comp vec reverse))
            (into {})))))
 
-(def temperature-lookup
-  (memoize
-    (fn []
-      (->> (lookup-grouping
-             (temperature-colors) (temperature-ranges) {:rev? true})
-           (map vec)
-           (into {})))))
+(defn elevation-check
+  [strides]
+  (cond (< strides elevation-min)
+        elevation-min
 
-(def temperature-rev-lookup
+        (> strides elevation-max)
+        elevation-max
+
+        :else
+        strides))
+
+(defn find-elevation-range
+  [strides]
+  (find-range (elevation-check strides) (elevation-ranges)))
+
+(defn elevation-color
+  [strides]
+  (get (elevation-lookup) (find-elevation-range strides)))
+
+(defn elevation-amount
+  "Given an RGB color hashmap, return the corresponding elevation."
+  [color-map]
+  (->> color-map
+       (get (elevation-rev-lookup))
+       ((fn [x] (log/trace "Elevation amount:" x) x))
+       (util/mean)))
+
+(defn print-elevation-colors
+  ([]
+    (print-elevation-colors elevation-min elevation-max 1000))
+  ([start stop step]
+    (let [output (mapv #(println (str (format "%,-6d strides : " %)
+                                      (util/color-map->ansi
+                                        (elevation-color %))))
+                       (range start (+ stop step) step))]
+      (println (string/join output)))))
+
+(defn coord->elevation
+  [im x y]
+  (log/debugf "Getting elevation for [%s, %s] ..." x y)
+  (-> (map-io/rgb im x y)
+      ((fn [x] (log/trace "RGB pixel:" x) x))
+      util/rgb-pixel->color-map
+      ((fn [x] (log/trace "Color map:" x) x))
+      elevation-amount))
+
+(defn elevation->pixel
+  [strides]
+  (-> strides
+      elevation-color
+      util/color-map->rgb-pixel))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Precipitation   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def precipitation-min 0) ; in mils/year (~4500 mm/yr)
+(def precipitation-max 640000) ; in mils/year, ~12,000 mil/week (~16,000 mm/yr)
+(def precipitation-file "001-mercator-precipitation-scale-hex")
+(def precipitation (read-scale-txt precipitation-file))
+(def precipitation-colors
   (memoize
-    (fn []
-      (->> (lookup-grouping
-             (temperature-colors) (temperature-ranges) {:rev? true})
-           (map (comp vec reverse))
-           (into {})))))
+    (fn [] (scale-colors-txt precipitation))))
+
+(def milyr-per-grade (float (/ (- precipitation-max precipitation-min)
+                                  (dec (count (precipitation-colors))))))
+
+(defn precipitation-ranges
+  ""
+  []
+  (get-ranges precipitation-min precipitation-max milyr-per-grade))
 
 (def precipitation-lookup
   (memoize
@@ -187,47 +188,20 @@
            (map (comp vec reverse))
            (into {})))))
 
-(defn find-range
-  [item collection]
-  (reduce (fn [_ [a b]] (when (<= a item b) (reduced [a b])))
-          []
-          collection))
+(defn precipitation-check
+  [milyr]
+  (cond (< milyr precipitation-min)
+        precipitation-min
 
-(defn find-elevation-range
-  [strides]
-  (find-range (elevation-check strides) (elevation-ranges)))
+        (> milyr precipitation-max)
+        precipitation-max
 
-(defn find-temperature-range
-  [kelvin]
-  (find-range (temperature-check kelvin) (temperature-ranges)))
+        :else
+        milyr))
 
 (defn find-precipitation-range
   [milyr]
   (find-range (precipitation-check milyr) (precipitation-ranges)))
-
-(defn elevation-color
-  [strides]
-  (get (elevation-lookup) (find-elevation-range strides)))
-
-(defn elevation-amount
-  "Given an RGB color hashmap, return the corresponding elevation."
-  [color-map]
-  (->> color-map
-       (get (elevation-rev-lookup))
-       ((fn [x] (log/trace "Elevation amount:" x) x))
-       (util/mean)))
-
-(defn temperature-color
-  [kelvin]
-  (get (temperature-lookup) (find-temperature-range kelvin)))
-
-(defn temperature-amount
-  "Given an RGB color hashmap, return the corresponding temperature."
-  [color-map]
-  (->> color-map
-       (get (temperature-rev-lookup))
-       ((fn [x] (log/trace "Temperature amount:" x) x))
-       (util/mean)))
 
 (defn precipitation-color
   [milyr]
@@ -241,53 +215,15 @@
        ((fn [x] (log/trace "Precipitation amount:" x) x))
        (util/mean)))
 
-(defn print-elevation-colors
-  ([]
-    (print-elevation-colors elevation-min elevation-max 1000))
-  ([start stop step]
-    (let [output (mapv #(println (str (format "%,-6d strides : " %)
-                                      (util/color-map->ansi
-                                        (elevation-color %))))
-                       (range start (+ stop step) step))]
-      (println (string/join output)))))
-
-(defn print-temperature-colors
-  ([]
-    (print-temperature-colors temperature-min temperature-max 3))
-  ([start stop step]
-    (let [output (mapv #(println (str (format "%,-3d K : " %)
-                                      (util/color-map->ansi
-                                        (temperature-color %))))
-                        (range start (+ stop step) step))]
-      (println (string/join output)))))
-
 (defn print-precipitation-colors
   ([]
-    (print-precipitation-colors precipitation-min precipitation-max 80000))
+    (print-precipitation-colors precipitation-min precipitation-max 40000))
   ([start stop step]
     (let [output (mapv #(println (str (format "%,-7d mils/year : " %)
                                       (util/color-map->ansi
                                         (precipitation-color %))))
                        (range start (+ stop step) step))]
       (println (string/join output)))))
-
-(defn coord->temperature
-  [im x y]
-  (log/debugf "Getting temperature for [%s, %s] ..." x y)
-  (-> (map-io/rgb im x y)
-      ((fn [x] (log/trace "RGB pixel:" x) x))
-      util/rgb-pixel->color-map
-      ((fn [x] (log/trace "Color map:" x) x))
-      temperature-amount))
-
-(defn coord->elevation
-  [im x y]
-  (log/debugf "Getting elevation for [%s, %s] ..." x y)
-  (-> (map-io/rgb im x y)
-      ((fn [x] (log/trace "RGB pixel:" x) x))
-      util/rgb-pixel->color-map
-      ((fn [x] (log/trace "Color map:" x) x))
-      elevation-amount))
 
 (defn coord->precipitation
   [im x y]
@@ -299,6 +235,94 @@
       precipitation-amount
       ((fn [x] (log/trace "Precipitation:" x) x))))
 
+(defn precipitation->pixel
+  [milyr]
+  (-> milyr
+      precipitation-color
+      util/color-map->rgb-pixel))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Temperature   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def temperature-min 212) ; in degrees C/K
+(def temperature-max 333) ; in degrees C/K
+(def temperature-file "001-mercator-temperature-scale-hex")
+(def temperature (read-scale-txt temperature-file))
+(def temperature-colors
+  (memoize
+    (fn [] (scale-colors-txt temperature))))
+
+(def degrees-per-grade (float (/ (- temperature-max temperature-min)
+                                 (dec (count (temperature-colors))))))
+
+(defn temperature-ranges
+  ""
+  []
+  (get-ranges temperature-min temperature-max degrees-per-grade))
+
+(def temperature-lookup
+  (memoize
+    (fn []
+      (->> (lookup-grouping
+             (temperature-colors) (temperature-ranges) {:rev? true})
+           (map vec)
+           (into {})))))
+
+(def temperature-rev-lookup
+  (memoize
+    (fn []
+      (->> (lookup-grouping
+             (temperature-colors) (temperature-ranges) {:rev? true})
+           (map (comp vec reverse))
+           (into {})))))
+
+(defn temperature-check
+  [kelvin]
+  (cond (< kelvin temperature-min)
+        temperature-min
+
+        (> kelvin temperature-max)
+        temperature-max
+
+        :else
+        kelvin))
+
+(defn find-temperature-range
+  [kelvin]
+  (find-range (temperature-check kelvin) (temperature-ranges)))
+
+(defn temperature-color
+  [kelvin]
+  (get (temperature-lookup) (find-temperature-range kelvin)))
+
+(defn temperature-amount
+  "Given an RGB color hashmap, return the corresponding temperature."
+  [color-map]
+  (->> color-map
+       (get (temperature-rev-lookup))
+       ((fn [x] (log/trace "Temperature amount:" x) x))
+       (util/mean)))
+
+(defn print-temperature-colors
+  ([]
+    (print-temperature-colors temperature-min temperature-max 3))
+  ([start stop step]
+    (let [output (mapv #(println (str (format "%,-3d K : " %)
+                                      (util/color-map->ansi
+                                        (temperature-color %))))
+                        (range start (+ stop step) step))]
+      (println (string/join output)))))
+
+(defn coord->temperature
+  [im x y]
+  (log/debugf "Getting temperature for [%s, %s] ..." x y)
+  (-> (map-io/rgb im x y)
+      ((fn [x] (log/trace "RGB pixel:" x) x))
+      util/rgb-pixel->color-map
+      ((fn [x] (log/trace "Color map:" x) x))
+      temperature-amount))
+
 (defn temperature->pixel
   [kelvin]
   (log/debugf "Getting pixel for temperature %s ..." kelvin)
@@ -306,16 +330,4 @@
       ((fn [x] (log/trace "Temperate:" x) x))
       temperature-color
       ((fn [x] (log/trace "Color map:" x) x))
-      util/color-map->rgb-pixel))
-
-(defn elevation->pixel
-  [strides]
-  (-> strides
-      elevation-color
-      util/color-map->rgb-pixel))
-
-(defn precipitation->pixel
-  [milyr]
-  (-> milyr
-      precipitation-color
       util/color-map->rgb-pixel))
