1248c65dfad22c83d875acc8cc0aa1a2231ded2d
Duncan McGreggor <oubiwann@gmail.com>
2019-12-24 15:18:08 -0600

    Refactored formatting code to share as much as possible.

--- src/hxgm30/map/biome/reporter.clj
+++ src/hxgm30/map/biome/reporter.clj
@@ -5,6 +5,10 @@
    [hxgm30.map.scales.core :as scales]
    [hxgm30.map.util :as util]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Shared Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn get-percent
   [fq total]
   (* 100 (float (/ fq total))))
@@ -12,14 +16,6 @@
 (defn format-percent
   [p]
   (format "%.1f%%" p))
-
-(defn format-precip
-  [p]
-  (format "%,d mm/yr" p))
-
-(defn format-temp
-  [k f]
-  (format "%d K (%d F)" k f))
 
 (defn get-frequency-data
   [total [rgb fq]]
@@ -31,6 +27,52 @@
      :hex (util/clj-hex->html (util/rgb-pixel->hex rgb))
      :ansi (util/color-map->ansi color-map)}))
 
+(defn get-stats
+  [im]
+  (let [rgbs-fqs (frequencies (sort (map-io/all-pixels im)))
+        total (reduce + (vals rgbs-fqs))
+        fdata (map (partial get-frequency-data total) rgbs-fqs)]
+    {:freqs fdata
+     :total total}))
+
+(defn print-data
+  ([stats header divider formatter]
+   (print-data stats header divider formatter {:sort-by :count}))
+  ([stats header divider formatter opts]
+   (let [fqs (reverse (sort-by (:sort-by opts) (:freqs stats)))
+         ]
+     (println header)
+     (println divider)
+     (doall
+      (for [fq fqs]
+        (println (formatter fq))))
+     (println (format "\nTotal counts: %d" (:total stats))))
+   :ok))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Precipitation Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def precip-format-str "%7s%8s%8s%8s %13s")
+(def precip-header (format precip-format-str
+                           "Color" "Hex" "Count" "Percent" "Precipitation"))
+(def precip-divider (format precip-format-str
+                            "-------" "-------" "-------" "-------"
+                            "-------------"))
+
+(defn precip-freq-formatter
+  [fq]
+  (format precip-format-str
+          (:ansi fq)
+          (:hex fq)
+          (:count fq)
+          (:percent fq)
+          (:precip-str fq)))
+
+(defn format-precip
+  [p]
+  (format "%,d mm/yr" p))
+
 (defn add-precip-datum
   [data]
   (let [precip (scales/precipitation-amount biome/ps (:color-map data))]
@@ -40,6 +82,40 @@
 (defn add-precip-data
   [stats]
   (assoc stats :freqs (map add-precip-datum (:freqs stats))))
+
+(defn get-precip-stats
+  [im]
+  (add-precip-data (get-stats im)))
+
+(defn print-precips
+  ([stats]
+   (print-precips stats {:sort-by :count}))
+  ([stats opts]
+   (print-data stats precip-header precip-divider precip-freq-formatter opts)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Temperature Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def temp-format-str "%7s%8s%8s%8s   %-12s")
+(def temp-header (format temp-format-str
+                         "Color" "Hex" "Count" "Percent" " Temperature"))
+(def temp-divider (format temp-format-str
+                          "-------" "-------" "-------" "-------"
+                          "------------"))
+
+(defn temp-freq-formatter
+  [fq]
+  (format temp-format-str
+          (:ansi fq)
+          (:hex fq)
+          (:count fq)
+          (:percent fq)
+          (:temp fq)))
+
+(defn format-temp
+  [k f]
+  (format "%d K (%d F)" k f))
 
 (defn add-temp-datum
   [data]
@@ -53,67 +129,19 @@
   [stats]
   (assoc stats :freqs (map add-temp-datum (:freqs stats))))
 
-(defn get-stats
-  [im]
-  (let [rgbs-fqs (frequencies (sort (map-io/all-pixels im)))
-        total (reduce + (vals rgbs-fqs))
-        fdata (map (partial get-frequency-data total) rgbs-fqs)]
-    {:freqs fdata
-     :total total}))
-
-(defn get-precip-stats
-  [im]
-  (add-precip-data (get-stats im)))
-
 (defn get-temp-stats
   [im]
   (add-temp-data (get-stats im)))
-
-;; XXX let's create a generalized function for these two, defining the following:
-;;     * precip/temp formatters
-;;     * precip/temp headers
-;;     * precip/temp dividers
-;;
-;;     These defs will then be passed to the general function, along with the key
-;;     to use for the type of report (e.g., :precip-str or :temp)
-
-(defn print-precips
-  ([stats]
-   (print-precips stats {:sort-by :count}))
-  ([stats opts]
-   (let [fqs (reverse (sort-by (:sort-by opts) (:freqs stats)))
-         format-str "%7s%8s%8s%8s %13s"]
-     (println (format format-str "Color" "Hex" "Count" "Percent" "Precipitation"))
-     (println (format format-str "-------" "-------" "-------" "-------" "-------------"))
-     (doall
-      (for [fq fqs]
-        (println (format format-str
-                         (:ansi fq)
-                         (:hex fq)
-                         (:count fq)
-                         (:percent fq)
-                         (:precip-str fq)))))
-     (println (format "\nTotal counts: %d" (:total stats))))
-   :ok))
 
 (defn print-temps
   ([stats]
    (print-temps stats {:sort-by :count}))
   ([stats opts]
-   (let [fqs (reverse (sort-by (:sort-by opts) (:freqs stats)))
-         format-str "%7s%8s%8s%8s   %-12s"]
-     (println (format format-str "Color" "Hex" "Count" "Percent" " Temperature"))
-     (println (format format-str "-------" "-------" "-------" "-------" "------------"))
-     (doall
-      (for [fq fqs]
-        (println (format format-str
-                         (:ansi fq)
-                         (:hex fq)
-                         (:count fq)
-                         (:percent fq)
-                         (:temp fq)))))
-     (println (format "\nTotal counts: %d" (:total stats))))
-   :ok))
+   (print-data stats temp-header temp-divider temp-freq-formatter opts)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Biome Functions   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; XXX Create a reporter for biome data as well as generated biome images:
 ;;     * Generalize the logic in biome.core/set-biome-pixel! to work with both
