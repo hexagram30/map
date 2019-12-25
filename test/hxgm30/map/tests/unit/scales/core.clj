310cd074af2877e4df8a7e0468add81621090f10
Duncan McGreggor <oubiwann@gmail.com>
2019-12-25 02:04:08 -0600

    Updated test value.

--- test/hxgm30/map/tests/unit/scales/core.clj
+++ test/hxgm30/map/tests/unit/scales/core.clj
@@ -7,9 +7,6 @@
    [clojure.test :refer :all]
    [hxgm30.map.scales.core :as scales])
   (:import
-   (hxgm30.map.scales.core ScaledRange
-                           PrecipitationRange
-                           TemperatureRange)
    (hxgm30.map.scales.precipitation LinearPrecipitationRange)
    (hxgm30.map.scales.temperature LinearTemperatureRange
                                   SineTemperatureRange)))
@@ -28,7 +25,7 @@
     (is (= 222 (scales/get-normalized-min ts)))
     ;; Check for the high-temp bug
     (let [color-map {:blue 2 :green 6 :red 83}]
-      (is (= 304 (scales/temperature-amount ts color-map)))))
+      (is (= 305 (scales/temperature-amount ts color-map)))))
   (let [ts (scales/new-scale :temperature :sine)]
     (is (record? ts))
     (is (= SineTemperatureRange (type ts) ))
